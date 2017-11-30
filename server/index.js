7a54ab9fc4a876ed3ef344e24a35d554d1f1f803
Kyle Uehlein <kyleuehlein@gmail.com>
2017-11-30 16:16:48 -0500

    feat: access control allow delete

--- server/index.js
+++ server/index.js
@@ -32,12 +32,14 @@ const createApp = () => {
   // logging middleware
   app.use(morgan('dev'))
 
-  // allow CORS
-  app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+ // allow CORS
+ app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'DELETE')
+  next()
+})
+
   // body parsing middleware
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
