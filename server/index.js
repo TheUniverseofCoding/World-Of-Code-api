867d34d64cfda1599e3dc5691f2ade34600a6f5c
Kyle Uehlein <kyleuehlein@gmail.com>
2017-12-01 02:59:48 -0500

    fix: trying to fix a bug

--- server/index.js
+++ server/index.js
@@ -34,9 +34,9 @@ const createApp = () => {
 
  // allow CORS
  app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.header('Access-Control-Allow-Methods', 'PUT', 'DELETE')
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "PUT", "DELETE")
   next()
 })
 
