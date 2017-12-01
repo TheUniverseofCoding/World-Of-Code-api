cebb850b5ef627f4b8af071b615d051c69fd81ac
Kyle Uehlein <kyleuehlein@gmail.com>
2017-12-01 11:41:19 -0500

    clean up comments

--- server/index.js
+++ server/index.js
@@ -35,13 +35,6 @@ app.use(morgan('dev'))
 
  // allow CORS
  app.use(cors())
-//  app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//   res.header('Access-Control-Allow-Methods', 'PUT', 'DELETE')
-//   // res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-//   next()
-// })
 
   // body parsing middleware
   app.use(bodyParser.json())
