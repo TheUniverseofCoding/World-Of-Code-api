45772d9eba6c0b9cfb3b9f6cedee1077d3b45a16
Kyle Uehlein <kyleuehlein@gmail.com>
2017-11-30 20:42:06 -0500

    fix: add model associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,20 +1,24 @@
 'use strict'
 
 const User = require('./user')
-const Input = require('./input')
 const Location = require('./location')
 const Question = require('./question')
+const UserQuestion = require('./userQuestion')
 
-
-// Location.belongsTo(User)
-// User.hasMany(Location)
 
 Location.hasMany(Question)
 Question.belongsTo(Location)
 
+Question.belongsTo(User)
+User.hasMany(Question)
+
+User.belongsToMany(Question, { through: UserQuestion })
+Question.belongsToMany(User, { through: UserQuestion })
+
+
 module.exports = {
   User,
-  Input,
   Location,
-  Question
+  Question,
+  UserQuestion
 }
