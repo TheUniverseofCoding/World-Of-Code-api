56ea87cb4bed8cfce565580e0f24d1acfc3875a9
apdjz <14apdjz@gmail.com>
2017-12-03 18:33:04 -0500

    minor change

--- scripts/seed.js
+++ scripts/seed.js
@@ -116,17 +116,17 @@ async function seed () {
     var dogs = animals.filter(isDog)
                       .map(selectedAnimal => selectedAnimal.name)
         console.log(dogs)`,
-        boilerplate: `const animals = [
-      {name: 'Fluffykins', species: 'rabbit'},
-      {name: 'Caro', species: 'dog'},
-      {name: 'Hamilton', species: 'dog'},
-      {name: 'Harold', species: 'fish'},
-      {name: 'Ursula', species: 'cat'},
-      {name: 'Jimmy', species: 'fish'},
-    ]
-    const isDog = animal => {
-      return animal.species === 'dog'
-    }`,
+        boilerplate: `var animals = [
+    {name: 'Fluffykins', species: 'rabbit'},
+    {name: 'Caro', species: 'dog'},
+    {name: 'Hamilton', species: 'dog'},
+    {name: 'Harold', species: 'fish'},
+    {name: 'Ursula', species: 'cat'},
+    {name: 'Jimmy', species: 'fish'},
+  ]
+  var isDog = animal => {
+    return animal.species === 'dog'
+  }`,
     locationId: 1,
     userId: 1
   }),
