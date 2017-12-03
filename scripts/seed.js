893b9448683044dbdf34db66286e3d517e7a8ace
apdjz <14apdjz@gmail.com>
2017-12-03 18:43:28 -0500

    style change

--- scripts/seed.js
+++ scripts/seed.js
@@ -103,20 +103,17 @@ async function seed () {
                   by species`,
     description: 'Video Exercise',
     answer: `var animals = [
-      {name: 'Fluffykins', species: 'rabbit'},
-      {name: 'Caro', species: 'dog'},
-      {name: 'Hamilton', species: 'dog'},
-      {name: 'Harold', species: 'fish'},
-      {name: 'Ursula', species: 'cat'},
-      {name: 'Jimmy', species: 'fish'},
-    ]
-    var isDog = animal => {
-      return animal.species === 'dog'
-    }
-    var dogs = animals.filter(isDog)
-                      .map(selectedAnimal => selectedAnimal.name)
-        console.log(dogs)`,
-        boilerplate: `var animals = [
+    {name: 'Fluffykins', species: 'rabbit'},
+    {name: 'Caro', species: 'dog'},
+    {name: 'Hamilton', species: 'dog'},
+    {name: 'Harold', species: 'fish'},
+    {name: 'Ursula', species: 'cat'},
+    {name: 'Jimmy', species: 'fish'},
+  ]
+  var dogs = animals.filter(function(animal) {
+    return animal.species === 'dog'
+  })`,
+    boilerplate: `var animals = [
     {name: 'Fluffykins', species: 'rabbit'},
     {name: 'Caro', species: 'dog'},
     {name: 'Hamilton', species: 'dog'},
