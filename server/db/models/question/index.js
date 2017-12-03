e1395a95a92121460940caab0edecfd6f230f9bc
Mariel Werner <marielwerner28@gmail.com>
2017-12-03 12:57:41 -0500

    change string length limit for description

--- server/db/models/question/index.js
+++ server/db/models/question/index.js
@@ -10,7 +10,7 @@ const Question = db.define('question', {
     allowNull: false
   },
   description: {
-    type: Sequelize.STRING(20),
+    type: Sequelize.STRING(50),
     allowNull: false
   },
   hints: Sequelize.ARRAY(Sequelize.TEXT),
