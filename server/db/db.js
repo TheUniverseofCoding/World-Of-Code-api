d997a85ee140c830dd0d0120b3d58793b77c2f28
apdjz <14apdjz@gmail.com>
2017-12-06 11:55:33 -0500

    added testing

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/world-of-code', {
+  process.env.DATABASE_URL || 'postgres://ctnhsnmsormaqo:a2d64b0a38868ae0256731f0e9d99d260ed543f057260b5f6f3ce731b1aac8d3@ec2-23-21-220-32.compute-1.amazonaws.com:5432/d6ebp59rqvafhn', {
     logging: false
   }
 )
